4d11a2620d066a513235e1d2063d3493498e1f1d
linuztx <linuztx@gmail.com>
2025-12-12 13:05:21 +0800

    migration: reload .env after moving to usr/ to update config

--- initialize.py
+++ initialize.py
@@ -141,9 +141,11 @@ def initialize_preload():
     return defer.DeferredTask().start_task(preload.preload)
 
 def initialize_migration():
-    from python.helpers import migration
+    from python.helpers import migration, dotenv
     # run migration
     migration.migrate_user_data()
+    # reload .env as it might have been moved
+    dotenv.load_dotenv()
     # reload settings to ensure new paths are picked up
     settings.reload_settings()
 
