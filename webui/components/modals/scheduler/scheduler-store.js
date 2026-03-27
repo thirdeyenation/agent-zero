68e65ae873691422635fedb9548ac7317732d68e
frdel <38891707+frdel@users.noreply.github.com>
2026-03-27 12:47:53 +0100

    fix scheduler api calls

--- webui/components/modals/scheduler/scheduler-store.js
+++ webui/components/modals/scheduler/scheduler-store.js
@@ -1,10 +1,10 @@
 import { createStore } from "/js/AlpineStore.js";
+import { fetchApi } from "/js/api.js";
 import { formatDateTime, getUserTimezone } from "/js/time-utils.js";
 import { store as chatsStore } from "/components/sidebar/chats/chats-store.js";
 import { store as projectsStore } from "/components/projects/projects-store.js";
 import { store as notificationsStore } from "/components/notifications/notification-store.js";
 
-const API = globalThis.fetchApi || globalThis.fetch;
 const VIEW_MODE_STORAGE_KEY = "scheduler_view_mode";
 const NOTIFICATION_DURATION = {
   success: 3,
@@ -268,7 +268,7 @@ function buildPayloadFromEditingTask(editingTask, { isCreating = false } = {}) {
 
 async function callSchedulerEndpoint(endpoint, payload = {}, defaultError) {
   try {
-    const response = await API(endpoint, {
+    const response = await fetchApi(endpoint, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
